982be677bbd158b6e730d8e7aac4fb7af83b7fcf
“fernandolba“ <fbarrios.pyc@gmail.com>
2026-08-13 01:18:02 -0500

    stripe payment success webhook added

---
+++ app/api/webhooks/stripe/route.ts
@@ -0,0 +1,38 @@
+import { NextRequest, NextResponse } from "next/server";
+import Stripe from "stripe";
+
+import { updateOrderToPaid } from "@/lib/actions/order.actions";
+import { STRIPE_SECRET_KEY } from "@/lib/constants";
+
+export async function POST(req: NextRequest) {
+  //? Build the webhook event
+  const event = Stripe.webhooks.constructEvent(
+    await req.text(),
+    req.headers.get("stripe-signature") as string,
+    STRIPE_SECRET_KEY as string,
+  );
+
+  //? Check for successfull payment
+  if (event.type === "charge.succeeded") {
+    const { object } = event.data;
+
+    //? Update order status
+    await updateOrderToPaid({
+      orderId: object.metadata.orderId,
+      paymentResult: {
+        id: object.id,
+        status: "COMPLETED",
+        email_address: object.billing_details.email!,
+        pricePaid: (object.amount / 100).toFixed(),
+      },
+    });
+
+    return NextResponse.json({
+      message: "updateOrderToPaid was successfull",
+    });
+  }
+
+  return NextResponse.json({
+    message: "Event is no succeeded",
+  });
+}
